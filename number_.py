2d9b50c19ab0a6f45a7a566edced4011019d3b26
WebberChen <s94492@gmail.com>
2019-03-01 17:35:10 +0800

    no.2

--- number_.py
+++ number_.py
@@ -1,25 +1,27 @@
-# 猜1 ~ 100 的數字
-#重覆猜
-# 猜對印出 猜對
-#猜錯要告訴他 大還小
 import random
 
-#usernum = int(input("請輸入1~100的數字"))
-comnum= random.randint(1,100)
+star= int(input("請輸入數字起始值:"))
+end=  int(input("請輸入數字結束值:"))
+comnum= random.randint(star,end)
 x=5
+
 while x>0 :
-    x = x-1
-    usernum = int(input("請重新輸入1~100的數字"))
-    if usernum == comnum :
-        print ("猜對了")
-        break
-    elif usernum < comnum :
-        print("太小了喔!")
-        if x>0 :
-            print ("您還剩下",x,"次機會")
-    elif usernum > comnum:
-        print ("太大了喔!")
-        if x > 0:
-            print("您還剩下", x, "次機會")
+    usernum = int(input("請輸入數字:"))
+
+    if usernum >= star and usernum <= end :
+        x = x - 1
+        if usernum == comnum :
+            print ("猜對了")
+            break
+        elif usernum < comnum :
+            print("太小了喔!")
+            if x>0 :
+                print ("您還剩下",x,"次機會")
+        elif usernum > comnum:
+            print ("太大了喔!")
+            if x > 0:
+                print("您還剩下", x, "次機會")
+    else:
+        print("請輸入",star,"~",end,"的數字")
 
 print ("遊戲結束")
